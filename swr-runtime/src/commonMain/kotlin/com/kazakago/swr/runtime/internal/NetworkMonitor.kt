0211675cadbf11c985ae5d6167a8fe63900b56ef
kazakago <kazakago@gmail.com>
2025-08-05 00:52:27 +0900

    fix

--- swr-runtime/src/commonMain/kotlin/com/kazakago/swr/runtime/internal/NetworkMonitor.kt
+++ swr-runtime/src/commonMain/kotlin/com/kazakago/swr/runtime/internal/NetworkMonitor.kt
@@ -5,6 +5,6 @@ import kotlinx.coroutines.flow.Flow
 public expect fun buildNetworkMonitor(): NetworkMonitor
 
 public interface NetworkMonitor {
-    public fun isOnline(): Boolean
     public val onlineStatusFlow: Flow<Boolean>
+    public fun isOnline(): Boolean
 }
--- swr-runtime/src/commonTest/kotlin/com/kazakago/swr/runtime/internal/TestNetworkMonitor.kt
+++ swr-runtime/src/commonTest/kotlin/com/kazakago/swr/runtime/internal/TestNetworkMonitor.kt
@@ -8,6 +8,6 @@ class TestNetworkMonitor(
     initialState: Boolean = true,
 ) : NetworkMonitor {
     val onlineStatus: MutableStateFlow<Boolean> = MutableStateFlow(initialState)
-    override val isOnline: Boolean = onlineStatus.value
     override val onlineStatusFlow: Flow<Boolean> = onlineStatus.drop(1)
+    override fun isOnline(): Boolean = onlineStatus.value
 }
