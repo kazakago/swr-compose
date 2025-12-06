3c72b3472f5462a7191db8c4e1d7644faa6996f7
kazakago <kazakago@gmail.com>
2025-12-06 23:56:28 +0900

    update dependencies

--- exampleApp/src/commonMain/kotlin/com/kazakago/swr/example/MainScreen.kt
+++ exampleApp/src/commonMain/kotlin/com/kazakago/swr/example/MainScreen.kt
@@ -22,6 +22,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.retain.RetainedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
